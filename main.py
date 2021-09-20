277c59dfb4f8c5508e24055f12e1d9cd97ed7b3e
quillaur <aurelienquillet@gmail.com>
2021-09-20 20:43:51 +0200

    Add the search through 118712 website. Data now shows as table with clickable links.

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import streamlit as st
 from selenium import webdriver
 from selenium.common import exceptions
 from selenium.webdriver.firefox.options import Options
-# import time
+import time
 import pandas as pd
+
+
+def make_clickable(url, text):
+    return f'<a target="_blank" href="{url}">{text}</a>'
 
 
 if __name__ == '__main__':
@@ -18,29 +22,66 @@ if __name__ == '__main__':
         with st.spinner(text=f"Search for the lastname '{lastname}' in progress..."):
             lastname = lastname.lower()
 
+            all_names = {}
+
+            # Set driver
             fp = webdriver.FirefoxProfile()
             options = Options()
             options.headless = True
             driver = webdriver.Firefox(firefox_profile=fp, options=options)
-            driver.get("https://www.pagesjaunes.fr/pagesblanches")
 
+            driver.get("https://www.pagesjaunes.fr/pagesblanches")
             driver.find_element_by_id("didomi-notice-agree-button").click()
             driver.find_element_by_id("ou").send_keys("Seine-Maritime (76)")
             driver.find_element_by_id("quoiqui").send_keys(lastname)
             driver.find_element_by_xpath("//button[@title='Trouver']").click()
 
-            all_names = []
             while True:
-                # # Get people names
-                all_names.extend([(t.get_attribute("title"), t.get_attribute("href")) for t in driver.find_elements_by_xpath("//a[contains(@class, 'denomination')]") if lastname in t.get_attribute("title").split()[0].lower()])
-                
+                # Get people names                
+                for elem in driver.find_elements_by_xpath("//a[contains(@class, 'denomination')]"):
+                    person_name = elem.get_attribute("title")
+                    lower_name = person_name.lower()
+                    if lastname in lower_name.split()[0]:
+                        all_names[lower_name] = {
+                            "Name": person_name,
+                            "URL Pages Blanches": make_clickable(elem.get_attribute("href"), "here"),
+                            "URL 118712": None
+                        }
+
                 try: 
                     driver.find_element_by_xpath("//*[@id='pagination-next']").click()
                 except exceptions.NoSuchElementException as e:
                     break
 
-        # Put data into dataframe
-        df = pd.DataFrame(all_names, columns=["Name", "URL"])
-        st.dataframe(df)
 
-        driver.quit()
+            driver.get("https://www.118712.fr/")
+            time.sleep(0.5)
+            driver.find_element_by_id("didomi-notice-agree-button").click()
+            driver.find_element_by_id("search_input_mono").send_keys(f"{lastname}, Seine-Maritime (76)")
+            driver.find_element_by_id("search_validation_normal").click()
+
+            for elem in driver.find_elements_by_xpath("//a[contains(@id, 'result')]"):
+                person_name = elem.get_attribute("title")
+                lower_name = person_name.lower()
+                if elem.is_displayed() and lastname in lower_name.split()[0]:
+                    if lower_name in all_names.keys():
+                        all_names[lower_name]["URL 118712"] = make_clickable(elem.get_attribute("href"), "here")
+                    else:
+                        all_names[lower_name] = {
+                            "Name": person_name,
+                            "URL Pages Blanches": None,
+                            "URL 118712": make_clickable(elem.get_attribute("href"), "here")
+                        }
+
+
+
+            driver.quit()
+
+            all_names = [d for k, d in all_names.items()]
+
+            df = pd.DataFrame(all_names)
+
+        # st.dataframe(df)
+        st.write(df.to_html(escape = False), unsafe_allow_html = True)
+
+        
--- selenium_search.py
+++ selenium_search.py
@@ -25,48 +25,60 @@ if __name__ == '__main__':
 
     fp = webdriver.FirefoxProfile()
     driver = webdriver.Firefox(firefox_profile=fp)
-    driver.get("https://www.pagesjaunes.fr/pagesblanches")
+    
+    # driver.get("https://www.pagesjaunes.fr/pagesblanches")
+    # driver.find_element_by_id("didomi-notice-agree-button").click()
+    # driver.find_element_by_id("ou").send_keys("Seine-Maritime (76)")
+    # driver.find_element_by_id("quoiqui").send_keys(lastname)
+    # driver.find_element_by_xpath("//button[@title='Trouver']").click()
 
+    # time.sleep(1)
+
+    # # all_names = {}
+    # while True:
+    #     # # Get people names
+    #     all_names = [(t.get_attribute("title"), t.get_attribute("href")) for t in driver.find_elements_by_xpath("//a[contains(@class, 'denomination-links')]") if lastname in t.get_attribute("title").split()[0].lower()]
+    #     print(len(all_names))
+    #     print(all_names[:5])
+
+    #     # # Get adresses
+    #     # all_adresses = [t.text.split("\n")[0] for t in driver.find_elements_by_xpath("//div[@class='main-adresse-container row-adresse with-adresse']")]
+    #     # print(len(all_adresses))
+    #     # print(all_adresses[:15])
+
+    #     # # Click on 'see phone numbers'      
+    #     # # for elem in driver.find_elements_by_xpath("//a[@class='phone pj-link']"):
+    #     # for elem in driver.find_elements_by_xpath("//*[contains(text(), 'Afficher le n°')]"):
+    #     #     elem.click()
+
+    #     # # Then get the phone numbers
+    #     # all_numbers = [t.text for t in driver.find_elements_by_class_name("num")]
+    #     # print(len(all_numbers))
+    #     # print(all_numbers[:15])
+
+    #     # Click on 'see phone numbers'      
+    #     # for elem in driver.find_elements_by_xpath("//a[@class='phone pj-link']"):
+    #     # for elem in driver.find_elements_by_xpath("//*[contains(text(), 'Afficher le n°') or contains(@class, 'phone')]"):
+    #     #     elem.click()
+
+
+
+    #     try: 
+    #         driver.find_element_by_xpath("//*[@id='pagination-next']").click()
+    #         time.sleep(1)
+    #     except exceptions.NoSuchElementException as e:
+    #         break
+
+
+    driver.get("https://www.118712.fr/")
+    time.sleep(0.5)
     driver.find_element_by_id("didomi-notice-agree-button").click()
-    driver.find_element_by_id("ou").send_keys("Seine-Maritime (76)")
-    driver.find_element_by_id("quoiqui").send_keys(lastname)
-    driver.find_element_by_xpath("//button[@title='Trouver']").click()
+    driver.find_element_by_id("search_input_mono").send_keys(f"{lastname}, Seine-Maritime (76)")
+    driver.find_element_by_id("search_validation_normal").click()
 
-    time.sleep(1)
+    # driver.find_element_by_id("propart-button").click()
+    # driver.find_element_by_id("ui-id-3").click()
 
-    # all_names = {}
-    while True:
-        # # Get people names
-        all_names = [(t.get_attribute("title"), t.get_attribute("href")) for t in driver.find_elements_by_xpath("//a[contains(@class, 'denomination-links')]") if lastname in t.get_attribute("title").split()[0].lower()]
-        print(len(all_names))
-        print(all_names[:5])
-
-        # # Get adresses
-        # all_adresses = [t.text.split("\n")[0] for t in driver.find_elements_by_xpath("//div[@class='main-adresse-container row-adresse with-adresse']")]
-        # print(len(all_adresses))
-        # print(all_adresses[:15])
-
-        # # Click on 'see phone numbers'      
-        # # for elem in driver.find_elements_by_xpath("//a[@class='phone pj-link']"):
-        # for elem in driver.find_elements_by_xpath("//*[contains(text(), 'Afficher le n°')]"):
-        #     elem.click()
-
-        # # Then get the phone numbers
-        # all_numbers = [t.text for t in driver.find_elements_by_class_name("num")]
-        # print(len(all_numbers))
-        # print(all_numbers[:15])
-
-        # Click on 'see phone numbers'      
-        # for elem in driver.find_elements_by_xpath("//a[@class='phone pj-link']"):
-        # for elem in driver.find_elements_by_xpath("//*[contains(text(), 'Afficher le n°') or contains(@class, 'phone')]"):
-        #     elem.click()
-
-
-
-        try: 
-            driver.find_element_by_xpath("//*[@id='pagination-next']").click()
-            time.sleep(1)
-        except exceptions.NoSuchElementException as e:
-            break
-
+    all_names = [(t.get_attribute("title"), t.get_attribute("href")) for t in driver.find_elements_by_xpath("//a[contains(@id, 'result')]") if t.is_displayed() and lastname in t.get_attribute("title").split()[0].lower()]
+    print(all_names)
     driver.quit()
