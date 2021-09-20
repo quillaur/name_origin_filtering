ebbf9eec222d7f742efc78354cfb440050d4d225
quillaur <aurelienquillet@gmail.com>
2021-09-20 13:44:45 +0200

    Get all people names matching the given lastname. Get url as well.

--- selenium_search.py
+++ selenium_search.py
@@ -1,37 +1,72 @@
+"""
+Usage: 
+    selenium_search.py  [-hl NAME]
+
+Options:
+    -h --help    show this-
+    -l --lastname NAME to search for [default: ""]
+"""
+
+
 from selenium import webdriver
 from selenium.common import exceptions
 import time
-
-
-def next_is_available()->bool:
-    try: 
-        driver.find_element_by_class_name("disabled next ")
-    except exceptions.NoSuchElementException as e:
-        return True
-
-    return False
+from docopt import docopt
 
 
 if __name__ == '__main__':
+    ###################
+    #### Arguments ####
+    ###################
+    args = docopt(__doc__)
+
+    lastname = args["--lastname"].lower()
+    print(f"Searching for {lastname}...")
+
     fp = webdriver.FirefoxProfile()
     driver = webdriver.Firefox(firefox_profile=fp)
     driver.get("https://www.pagesjaunes.fr/pagesblanches")
 
     driver.find_element_by_id("didomi-notice-agree-button").click()
     driver.find_element_by_id("ou").send_keys("Seine-Maritime (76)")
-    driver.find_element_by_id("quoiqui").send_keys("MIN")
+    driver.find_element_by_id("quoiqui").send_keys(lastname)
     driver.find_element_by_xpath("//button[@title='Trouver']").click()
 
     time.sleep(1)
 
-    all_names = {}
+    # all_names = {}
     while True:
-        all_names.extend([t.text for t in driver.find_elements_by_xpath("//li/div/header/div[1]/div/h3/a")])
+        # # Get people names
+        all_names = [(t.text, t.get_attribute("href")) for t in driver.find_elements_by_xpath("//a[contains(@class, 'denomination-links')]") if lastname in t.text.split()[0].lower()]
         print(len(all_names))
-        print(all_names[:15])
+        print(all_names[:5])
 
-        if next_is_available():
+        # # Get adresses
+        # all_adresses = [t.text.split("\n")[0] for t in driver.find_elements_by_xpath("//div[@class='main-adresse-container row-adresse with-adresse']")]
+        # print(len(all_adresses))
+        # print(all_adresses[:15])
+
+        # # Click on 'see phone numbers'      
+        # # for elem in driver.find_elements_by_xpath("//a[@class='phone pj-link']"):
+        # for elem in driver.find_elements_by_xpath("//*[contains(text(), 'Afficher le n°')]"):
+        #     elem.click()
+
+        # # Then get the phone numbers
+        # all_numbers = [t.text for t in driver.find_elements_by_class_name("num")]
+        # print(len(all_numbers))
+        # print(all_numbers[:15])
+
+        # Click on 'see phone numbers'      
+        # for elem in driver.find_elements_by_xpath("//a[@class='phone pj-link']"):
+        # for elem in driver.find_elements_by_xpath("//*[contains(text(), 'Afficher le n°') or contains(@class, 'phone')]"):
+        #     elem.click()
+
+
+
+        try: 
             driver.find_element_by_xpath("//*[@id='pagination-next']").click()
             time.sleep(1)
-        else:
+        except exceptions.NoSuchElementException as e:
             break
+
+    driver.quit()
